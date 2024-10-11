b635c7c2300bbd112ad245a3112a107dc5d3e43f
SumitPandey08 <sp.sumitpandey06@gmail.com>
2024-10-11 13:18:18 +0530

    Bg Color Changer

--- bgColor/bgColor/src/App.jsx
+++ bgColor/bgColor/src/App.jsx
@@ -32,12 +32,12 @@ function App() {
    <div className='flex flex-wrap gap-3 shadow-xl bg-white px-3 py-2 rounded-2xl'>
    <button onClick={green} className='bg-green-600 px-4 py-1 rounded-lg'>Green</button>
    <button onClick={red} className='bg-red-600 px-4 py-1 rounded-lg'>Red</button>
-   <button onClick={olive}className='bg-olive px-4 py-1 rounded-lg'>Olive</button>
-   <button onClick={blue} className='bg-blue-600 px-4 py-1 rounded-lg'>Blue</button>
+   <button onClick={olive}className='bg-olive px-4 py-1 rounded-lg' style={{backgroundColor:"olive"}}>Olive</button>
+   <button onClick={blue} className='bg-blue-600 px-4 py-1 rounded-lg' >Blue</button>
    <button onClick={black} className='bg-black px-4 py-1 rounded-lg'
   >Black</button>
    <button onClick={white} className='bg-white px-4 py-1 rounded-lg'>White</button>
-   <button onClick={golden} className='bg-golden px-4 py-1 rounded-lg'>Golden</button>
+   <button onClick={golden} className='bg-golden px-4 py-1 rounded-lg' style={{backgroundColor:"gold"}}>Golden</button>
    </div>
     </div>
     </div>
